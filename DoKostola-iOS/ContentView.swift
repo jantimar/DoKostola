3ce4f3d902335c90d4ab73dd8d73306ea68494da
Jan Timar <jantimar2@gmail.com>
2020-02-24 20:09:10 +0100

    Initial commit

---
+++ DoKostola-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DoKostola-iOS
+//
+//  Created by Jan Timar on 24/02/2020.
+//  Copyright © 2020 Jan Timar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
