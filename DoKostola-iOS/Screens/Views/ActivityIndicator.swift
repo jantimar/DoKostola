90453674f8e23f05fed617cb23bc2561e2f4325b
Jan Timar <jantimar2@gmail.com>
2020-03-15 12:43:56 +0100

    Add ActivityIndicator View

---
+++ DoKostola-iOS/Screens/Views/ActivityIndicator.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+
+	@Binding var isAnimating: Bool
+	let style: UIActivityIndicatorView.Style
+
+	// MARK: - UIViewRepresentable
+	typealias UIViewType = UIActivityIndicatorView
+
+	func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> ActivityIndicator.UIViewType {
+		return UIActivityIndicatorView(style: style)
+	}
+
+	func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+		isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+	}
+}
+
+struct AcivityIndicator_Previews: PreviewProvider {
+	static var previews: some View {
+		return ActivityIndicator(
+			isAnimating: .constant(true),
+			style: .large
+		)
+	}
+}
