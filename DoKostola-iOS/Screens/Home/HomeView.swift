42aac9d3c9440612a60878f33eca315baeca1afe
Jan Timar <jantimar2@gmail.com>
2020-03-17 15:04:04 +0100

    Add Search and Info navigation link to HomeView

--- DoKostola-iOS/Screens/Home/HomeView.swift
+++ DoKostola-iOS/Screens/Home/HomeView.swift
@@ -6,14 +6,35 @@ struct HomeView: View {
 	@ObservedObject var viewModel: HomeViewModel
 
     var body: some View {
-        Text("Home")
+		NavigationView {
+			VStack {
+				Text("Home")
+			}
+			.navigationBarItems(
+				leading: leadingBarButtons,
+				trailing: trialingBarButtons
+			)
+			.navigationBarTitle("DoKostola.sk")
+		}
     }
+
+	private var leadingBarButtons: some View {
+		NavigationLink(destination: EmptyView()) {
+			Text("Info")
+		}
+	}
+	
+	private var trialingBarButtons: some View {
+		NavigationLink(destination: EmptyView()) {
+			Text("Search")
+		}
+	}
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
 		let apiService = DoKostolaAPIService()
-		let viewModel = HomeViewModel(apiService: apiService)
+		let viewModel = HomeViewModel(apiService: apiService, repo: Repo())
 		return HomeView(viewModel: viewModel)
     }
 }
--- DoKostola-iOS/Screens/Home/HomeViewModel.swift
+++ DoKostola-iOS/Screens/Home/HomeViewModel.swift
@@ -3,9 +3,15 @@ import Combine
 
 final class HomeViewModel: ObservableObject {
 
-	private let apiService: DoKostolaAPIServiceProtocol
-	private let locationManager: LocationManager
-	private var disposables = Set<AnyCancellable>()
+	/// Date for show `messes`, default value is today, can be set to date in future
+	@Published var date = Date()
+	/// Distance in `km` to show nearst `churches`
+	@Published var distance = 25
+	/// All churches in `distance`in `distacne` from current location
+	@Published var churches = [Church]()
+
+	var showInfo: (() -> Void)?
+	var showSearch: (() -> Void)?
 
 	init(
 		apiService: DoKostolaAPIServiceProtocol,
@@ -13,9 +19,17 @@ final class HomeViewModel: ObservableObject {
 		locationManager: LocationManager = LocationManager()
 	) {
 		self.apiService = apiService
+		self.repo = repo
 		self.locationManager = locationManager
 		self.setup()
 	}
+
+	// MARK: - Private
+
+	private let apiService: DoKostolaAPIServiceProtocol
+	private let repo: Repo
+	private let locationManager: LocationManager
+	private var disposables = Set<AnyCancellable>()
 
 	private func setup() {
 		
