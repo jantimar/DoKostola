bf2461dab784e50896b0a88c863739524c156615
Jan Timar <jantimar2@gmail.com>
2020-03-27 11:31:35 +0100

    Implement Search functionality in SearchViewModel

--- DoKostola-iOS/Screens/Search/SearchView.swift
+++ DoKostola-iOS/Screens/Search/SearchView.swift
@@ -12,13 +12,80 @@ struct SearchView: View {
 
 	@ObservedObject var viewModel: SearchViewModel
 
-    var body: some View {
-        Text("Search view")
-    }
+	var body: some View {
+		VStack {
+			SearchTextField(text: $viewModel.search)
+				.frame(height: 40)
+				.padding([.trailing, .leading], 16)
+			List(viewModel.rows) { row in
+				SearchRowView(row: row)
+			}
+		}
+		.navigationBarTitle("Search")
+	}
+}
+
+struct SearchRowView: View {
+	var row: SearchRow
+
+	var body: some View {
+		switch row {
+		case let .church(church):
+			return AnyView(ChurchRow(church: church))
+		case let .city(city):
+			return AnyView(CityRow(city: city))
+		case let .text(text):
+			return AnyView(Text(text))
+		}
+	}
+}
+
+struct CityRow: View {
+	var city: City
+
+	var body: some View {
+		Text(city.title)
+	}
+}
+
+struct ChurchRow: View {
+	var church: Church
+
+	var body: some View {
+		Text(church.title)
+	}
 }
 
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
-        SearchView(viewModel: SearchViewModel())
+
+		let city = CityDTO(
+			title: "city1",
+			city: "1",
+			lng: "0",
+			lat: "0"
+		)
+		let church = ChurchDTO(
+			church: "church1",
+			title: "church1",
+			city: "city1",
+			lat: "0",
+			lng: "0",
+			desc: "desc",
+			image: nil,
+			thumbnail: nil,
+			alias: "alias"
+		)
+
+		let repo = Repo()
+		repo.churches = [Church(churchDTO: church)]
+		repo.cities = [City(cityDTO: city)]
+
+		let viewModel = SearchViewModel(repo: repo)
+		let view = SearchView(viewModel: viewModel)
+
+		// Search `c`
+		viewModel.search = "c"
+		return view
     }
 }
