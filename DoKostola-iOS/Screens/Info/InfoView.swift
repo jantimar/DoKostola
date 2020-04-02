be20687f5aee1943e683ff35defd8320bafa9c0c
Jan Timar <jantimar2@gmail.com>
2020-04-02 17:22:22 +0200

    Update Info screen with dummy elements

--- DoKostola-iOS/Screens/Info/InfoView.swift
+++ DoKostola-iOS/Screens/Info/InfoView.swift
@@ -1,11 +1,3 @@
-//
-//  InfoView.swift
-//  DoKostola
-//
-//  Created by Jan Timar on 25/03/2020.
-//  Copyright © 2020 Jan Timar. All rights reserved.
-//
-
 import SwiftUI
 
 struct InfoView: View {
@@ -13,8 +5,38 @@ struct InfoView: View {
 	@ObservedObject var viewModel: InfoViewModel
 
     var body: some View {
-        Text("Info view")
+		VStack {
+            ForEach(viewModel.texts, id: \.self) {
+                Text($0)
+                    .padding()
+                    .multilineTextAlignment(.center)
+            }
+
+			Spacer()
+			Button(action: {}) {
+				Text("Report bug in feast")
+			}.buttonStyle(DefaultButtonStyle())
+			Button(action: {}) {
+				Text("Open DoKostola.sk")
+			}.buttonStyle(DefaultButtonStyle())
+			Button(action: {}) {
+				Text("Contact app author")
+			}.buttonStyle(DefaultButtonStyle())
+		}
+		.padding()
+		.navigationBarTitle("Info")
     }
+}
+
+private struct DefaultButtonStyle: ButtonStyle {
+	func makeBody(configuration: Self.Configuration) -> some View {
+		configuration.label
+			.padding()
+			.foregroundColor(.white)
+			.background(configuration.isPressed ? Color.gray : Color.black)
+			.cornerRadius(4)
+			.padding()
+	}
 }
 
 struct InfoView_Previews: PreviewProvider {
--- DoKostola-iOS/Screens/Info/InfoViewModel.swift
+++ DoKostola-iOS/Screens/Info/InfoViewModel.swift
@@ -2,5 +2,9 @@ import Foundation
 import Combine
 
 final class InfoViewModel: ObservableObject {
-
+	var texts = [
+		"Aplikácia DoKostola Vám pomôže nájsť bohoslužby vo vašom okolí.",
+		"Cez jednoduché vyhľadávanie nájdete ktorýkoľvek kostol Katolíckej cirkvi na Slovensku vrátane adresy, časov bohoslužieb, sviatkov a možnosti navigovať.",
+		"iOS applikáciu vytvoril: Ján Timár pre Admissam, o.z. = prevadzkovateľa portálu DoKostola.sk"
+	]
 }
