dfe24bd540095eb139e58a0b3157c7809029ebf9
Jan Timar <jantimar2@gmail.com>
2020-04-04 17:24:40 +0200

    Update dateFormatter tests with hour-minute-seconds test

--- DoKostola-iOS/Extensions/DateFormatter+TypesTests.swift
+++ DoKostola-iOS/Extensions/DateFormatter+TypesTests.swift
@@ -4,20 +4,38 @@ import XCTest
 final class DateFormatter_TypesTests: XCTestCase {
 
 	func testParseYearMonthDay() {
-		let formatter: DateFormatter = .yearMonthDay
-		guard let date = formatter.date(from: "2020-03-15") else {
-			XCTFail("Unexpected state")
-			return
-		}
+        let formatter: DateFormatter = .yearMonthDay
+        guard let date = formatter.date(from: "2020-03-15") else {
+            XCTFail("Unexpected state")
+            return
+        }
 
-		let calendar = Calendar(identifier: .gregorian)
-		let components = calendar.dateComponents(
-			[.year, .month, .day],
-			from: date
-		)
-		XCTAssertEqual(2020, components.year)
-		XCTAssertEqual(3, components.month)
-		XCTAssertEqual(15, components.day)
-	}
+        let calendar = Calendar(identifier: .gregorian)
+        let components = calendar.dateComponents(
+            [.year, .month, .day],
+            from: date
+        )
+        XCTAssertEqual(2020, components.year)
+        XCTAssertEqual(3, components.month)
+        XCTAssertEqual(15, components.day)
+        XCTAssertEqual("2020-03-15", formatter.string(from: date))
+    }
 
+    func testParseHourMinutesSeconds() {
+        let formatter: DateFormatter = .hourMinutesSeconds
+        guard let date = formatter.date(from: "17:21:04") else {
+            XCTFail("Unexpected state")
+            return
+        }
+
+        let calendar = Calendar(identifier: .gregorian)
+        let components = calendar.dateComponents(
+            [.hour, .minute, .second],
+            from: date
+        )
+        XCTAssertEqual(17, components.hour)
+        XCTAssertEqual(21, components.minute)
+        XCTAssertEqual(4, components.second)
+        XCTAssertEqual("17:21:04", formatter.string(from: date))
+    }
 }
