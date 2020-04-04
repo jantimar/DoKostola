a2748fa611ac4b6414f98d33a8fb03cff4bd3cca
Jan Timar <jantimar2@gmail.com>
2020-04-04 12:13:45 +0200

    Add time date formatter

--- DoKostola-iOS/Extensions/DateFormatter+Types.swift
+++ DoKostola-iOS/Extensions/DateFormatter+Types.swift
@@ -1,11 +1,17 @@
 import Foundation
 
 extension DateFormatter {
-	/// Date formatter with `"YYYY-MM-dd"` format
+	/// Date formatter with `YYYY-MM-dd` format
 	static var yearMonthDay: DateFormatter = {
 		let formatter = DateFormatter()
 		formatter.dateFormat = "yyyy-MM-dd"
 		return formatter
 	}()
 
+    /// Date formatter with `HH:mm:ss` format
+    static var hourMinutesSeconds: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        return formatter
+    }()
 }
