1b4b89ee6cf7422aa9c3fd443a39f603acad09de
Jan Timar <jantimar2@gmail.com>
2020-04-04 12:18:48 +0200

    Clean LocationManager

--- DoKostola-iOS/Managers/LocationManager/LocationManager.swift
+++ DoKostola-iOS/Managers/LocationManager/LocationManager.swift
@@ -2,13 +2,14 @@ import Foundation
 import CoreLocation
 import Combine
 
+/// Combine wrapper for CLLocationManager
 final class LocationManager: NSObject {
 
+    /// Combine publisher for update CLLocation or Error when occure
 	let publisher: AnyPublisher<CLLocation, Error>
 
-	private let locationManager: CLLocationManager
-	private let subject = PassthroughSubject<CLLocation, Error>()
-
+    /// Create new CLLotacitonManager and ask for authorize permission if needed, if have or get permission start updating location
+    /// - Parameter locationManager: CLLocationManager
 	init(locationManager: CLLocationManager = .init()) {
 		self.locationManager = locationManager
 		self.publisher = subject.eraseToAnyPublisher()
@@ -17,21 +18,26 @@ final class LocationManager: NSObject {
 		self.locationManager.requestWhenInUseAuthorization()
 	}
 
+    /// Start update location, this method is call when init `LocationManager` or get authorize permission
 	func start() {
 		locationManager.startUpdatingLocation()
 	}
 
+    /// Stop updating location manager, call this method when you don't want any update anymore
 	func stop() {
 		locationManager.stopUpdatingLocation()
 	}
+
+    // MARK: - Private
+
+    private let locationManager: CLLocationManager
+    private let subject = PassthroughSubject<CLLocation, Error>()
 }
 
+// MARK: - CLLocationManagerDelegate
 extension LocationManager: CLLocationManagerDelegate {
 	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-		guard let location = locations.first else {
-			return
-		}
-
+		guard let location = locations.first else { return }
 		subject.send(location)
 	}
 
